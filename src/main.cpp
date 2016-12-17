645dccb272b6d4122c119379cd64afb905ee78b2
Eric <eric@solutionsfortomorrow.se>
2016-12-17 22:34:58 +0100

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,9 @@ String events[100];
 unsigned int nrEvents = 0;
 bool _stringComplete = false;
 char _dataRec[30];
+bool _set = false;
+bool _get = false;
+bool _time = false;
 
 void setup()
 {
@@ -117,18 +120,124 @@ void handleSerial()
     {
         if((_dataRec[0] == 'g') && (_dataRec[1] == 'e') && (_dataRec[2] == 't'))
         {
-            for(int i=0;i<nrEvents;i++)
+            for(unsigned int i=0;i<nrEvents;i++)
             {
                 Serial.println(events[i]);
             }
             Serial.println();
         }
-        else if((_dataRec[0] == 'r') && (_dataRec[1] == 'e') && (_dataRec[2] == 's') && (_dataRec[2] == 'e') && (_dataRec[2] == 't'))
+        else if((_dataRec[0] == 'r') && (_dataRec[1] == 'e') && (_dataRec[2] == 's') && (_dataRec[3] == 'e') && (_dataRec[4] == 't'))
         {
             nrEvents = 0;
         }
-    }
-    _stringComplete = false;
+        else if((_dataRec[0] == 'h') && (_dataRec[1] == 'e') && (_dataRec[2] == 'l') && (_dataRec[3] == 'p'))
+        {
+            Serial.println("Examples of valid commands:");
+            Serial.println("get #prints all events");
+            Serial.println("reset #resets event counter");
+
+            Serial.println("s t 22:01 16.02.30 #set time to 22:01 2016-02-30");
+            Serial.println("g t #get time currently set");
+            Serial.println("");
+        }
+        else
+        {
+             if(_dataRec[0] == 's')
+             {
+                 _set = true;
+             }
+             else if(_dataRec[0] == 'g')
+             {
+                 _get = true;
+             }
+             else
+             {
+                 Serial.print("Command error\n");
+             }
+
+             if(_dataRec[2] == 't')
+             {
+                 _time = true;
+             }
+             else
+             {
+                 Serial.print("Command error\n");
+             }
+
+             if(_set && _time)
+             {
+                 int iHour = 0;
+                 int iMinute = 0;
+                 int iYear = 0;
+                 int iMonth = 0;
+                 int iDay = 0;
+
+                 char tmpMin[3];
+                 tmpMin[0] = _dataRec[7];
+                 tmpMin[1] = _dataRec[8];
+                 char tmpHour[3];
+                 tmpHour[0] = _dataRec[4];
+                 tmpHour[1] = _dataRec[5];
+                 char tmpYear[3];
+                 tmpYear[0] = _dataRec[10];
+                 tmpYear[1] = _dataRec[11];
+                 char tmpMonth[3];
+                 tmpMonth[0] = _dataRec[13];
+                 tmpMonth[1] = _dataRec[14];
+                 char tmpDay[3];
+                 tmpDay[0] = _dataRec[16];
+                 tmpDay[1] = _dataRec[17];
+
+                 iHour = atoi(tmpHour);
+                 iMinute = atoi(tmpMin);
+                 iYear = atoi(tmpYear);
+                 iMonth = atoi(tmpMonth);
+                 iDay = atoi(tmpDay);
+
+                 if(((iHour >= 0) && (iHour <= 23)) && ((iMinute >= 0) && (iMinute <= 59)))
+
+                 {
+                     if(_time)
+                     {
+                         if(((iYear >= 0) && (iYear <= 99)) && ((iMonth >= 1) && (iMonth <= 12)) && ((iDay >= 1) && (iDay <= 31)))
+                         {
+                             clock.Hour = iHour;
+                             clock.Minute = iMinute;
+                             clock.Year = (iYear + 2000) - 1970;
+                             clock.Month = iMonth;
+                             clock.Day = iDay;
+
+                             RTC.write(clock);
+                         }
+                     }
+                 }
+                 _set = false;
+                 _time = false;
+             }
+             else if(_get && _time)
+             {
+                 Serial.print("Get Time: ");
+                 Serial.print(zeroPad(clock.Hour,2));
+                 Serial.print(":");
+                 Serial.print(zeroPad(clock.Minute,2));
+                 Serial.print(":");
+                 Serial.print(zeroPad(clock.Second,2));
+
+                 Serial.print(" ");
+                 Serial.print(zeroPad(1970+clock.Year,4));
+                 Serial.print("-");
+                 Serial.print(zeroPad(clock.Month,2));
+                 Serial.print("-");
+                 Serial.print(zeroPad(clock.Day,2));
+                 Serial.println();
+                 _get = false;
+                 _time = false;
+             }
+
+         }
+         _stringComplete = false;
+         ii = 0;
+     }
 }
 
 void loop()
